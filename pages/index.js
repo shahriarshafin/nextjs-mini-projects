6fb2b48a10e1705a00cebed3363b3f56067e3654
shafin <shafinshahriar340@gmail.com>
2022-08-20 00:20:05 +0600

    form post added

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,68 @@
 const Index = () => {
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const data = {
+			name: e.target.username.value,
+			password: e.target.password.value,
+		};
+		const JSONdata = JSON.stringify(data);
+
+		const response = await fetch('http://localhost:4000/posts', {
+			body: JSONdata,
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			method: 'POST',
+		});
+	};
+
 	return (
 		<>
-			<h1 className='text-3xl font-bold underline text-center'>Hello world!</h1>
+			<div className='mx-auto w-full max-w-xs'>
+				<form
+					onSubmit={handleSubmit}
+					className='mt-20 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'
+				>
+					<div className='mb-4'>
+						<label
+							className='block text-gray-700 text-sm font-bold mb-2'
+							htmlFor='username'
+						>
+							Username
+						</label>
+						<input
+							className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+							id='username'
+							type='text'
+							name='username'
+							placeholder='Username'
+						/>
+					</div>
+					<div className='mb-6'>
+						<label
+							className='block text-gray-700 text-sm font-bold mb-2'
+							htmlFor='password'
+						>
+							Password
+						</label>
+						<input
+							className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+							id='password'
+							type='password'
+							name='password'
+							placeholder='******************'
+						/>
+					</div>
+					<div className='flex items-center justify-between'>
+						<button
+							className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+							type='submit'
+						>
+							Sign In
+						</button>
+					</div>
+				</form>
+			</div>
 		</>
 	);
 };
