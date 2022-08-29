f2ee3520d3a6a2f0579c020c318c77f6537dfaf0
shafin <shafinshahriar340@gmail.com>
2022-08-29 17:59:53 +0600

    added user to database

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,18 +1,22 @@
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import NextAuth from 'next-auth';
-import GitHubProvider from "next-auth/providers/github";
+import GitHubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
+import clientPromise from '../../../lib/mongodb';
 export default NextAuth({
 	// Configure one or more authentication providers
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-		}), GitHubProvider({
+		}),
+		GitHubProvider({
 			clientId: process.env.GITHUB_ID,
-			clientSecret: process.env.GITHUB_SECRET
-		  })
+			clientSecret: process.env.GITHUB_SECRET,
+		}),
 		// ...add more providers here
 	],
+	adapter: MongoDBAdapter(clientPromise),
 	// session: {
 	// 	jwt: true, // by default true
 	// 	maxAge: 5,  // user automatic loged out after 5 sec
